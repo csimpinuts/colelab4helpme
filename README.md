81bce3db3ba28f65c43684156e092b2ecb29310c
csimpinuts <simpsonc2@etsu.edu>
2024-10-16 17:56:16 -0400

    bare bones
    this has used the api in the ways id like to use it in. the only problems with this build would be the way that things look.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Lab 4. please help me.
 
 So far, my only problems have been providing the correct information to actually log in. Another problem which I still haven't found a solution to is my inability to push changes from my local clone to the main repository.
+
+Second update. Note: I forgot to commit for this milestone. My problems here were caused by some unknown force. I was working on the display for the time frames of stats you can see and the page just wouldnt load. I just Ctrl Z'ed my way back to a version where it did work and tried a different way to have my little play buttons. I will say I did have a bit of help from my buddy chat, especially with the areas im not familiar with or haven't touched in a while like html and java.
+
+Some more problems ive faced is my desire to get a currently playing song display that updates when any of the buttons are pressed. These problems are more associated with trying to get the look of the program to be what i want it to be. At this point i think i've utilized the api in the ways i would like.
--- flaskserver.py
+++ flaskserver.py
@@ -11,13 +11,35 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
 
+def get_current_playback(access_token):
+    playback_url = 'https://api.spotify.com/v1/me/player/currently-playing'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    playback_response = requests.get(playback_url, headers=headers)
+
+    if playback_response.status_code == 200:
+        current_playback = playback_response.json()
+        if current_playback and 'item' in current_playback:
+            song_name = current_playback['item']['name']
+            artist_name = current_playback['item']['artists'][0]['name']
+            album_name = current_playback['item']['album']['name']
+            album_cover = current_playback['item']['album']['images'][0]['url']
+            return {
+                "song_name": song_name,
+                "artist_name": artist_name,
+                "album_name": album_name,
+                "album_cover": album_cover
+            }
+    return None
+
 @app.route('/')
 def index():
     # Spotify login
     auth_url = (
         f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}"
         f"&response_type=code&redirect_uri={REDIRECT_URI}"
-        f"&scope=user-read-playback-state,user-modify-playback-state"
+        f"&scope=user-read-playback-state,user-modify-playback-state,user-top-read"
     )
     return redirect(auth_url)
 
@@ -32,9 +54,10 @@ def callback():
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET
     }
+    
     response = requests.post(token_url, data=token_data)
     if response.status_code != 200:
-        return f"Error: {response.json().get('error_description')}"
+        return f"Error: {response.json().get('error_description', 'Unknown error')}"
     
     access_token = response.json().get('access_token')
 
@@ -45,24 +68,125 @@ def callback():
     }
     playback_response = requests.get(playback_url, headers=headers)
 
+    # Print the raw response for debugging
+    print("Playback response status code:", playback_response.status_code)
+    print("Playback response content:", playback_response.text)
+
     if playback_response.status_code == 200:
         current_playback = playback_response.json()
-        song_name = current_playback['item']['name']
-        artist_name = current_playback['item']['artists'][0]['name']
-        album_name = current_playback['item']['album']['name']
-        album_cover = current_playback['item']['album']['images'][0]['url']
 
-        return f"""
-        <h1>Now Playing</h1>
-        <p>Song: {song_name}</p>
-        <p>Artist: {artist_name}</p>
-        <p>Album: {album_name}</p>
-        <img src="{album_cover}" alt="Album Cover" style="width:200px;height:auto;">
-        <br>
-        <a href="/play/{current_playback['item']['id']}?access_token={access_token}">Play this track again</a>
-        """
+        # Check if there's currently playing content
+        if 'item' in current_playback:
+            song_name = current_playback['item']['name']
+            artist_name = current_playback['item']['artists'][0]['name']
+            album_name = current_playback['item']['album']['name']
+            album_cover = current_playback['item']['album']['images'][0]['url']
+
+            return f"""
+            <h1>Now Playing</h1>
+            <p>Song: {song_name}</p>
+            <p>Artist: {artist_name}</p>
+            <p>Album: {album_name}</p>
+            <img src="{album_cover}" alt="Album Cover" style="width:200px;height:auto;">
+            <br>
+            <a href="/play/{current_playback['item']['id']}?access_token={access_token}">Play this track again</a>
+            <br>
+            <a href="/top?access_token={access_token}&time_range=1mo">View Top Songs and Artists (Last Month)</a>
+            <br>
+            <a href="/top?access_token={access_token}&time_range=6mo">View Top Songs and Artists (Last 6 Months)</a>
+            <br>
+            <a href="/top?access_token={access_token}&time_range=12mo">View Top Songs and Artists (All Time)</a>
+            """
+        else:
+            return "<h1>No song is currently playing.</h1>"
     else:
-        return f"Error retrieving playback state: {playback_response.json().get('error_description')}"
+        return f"Error retrieving playback state: {playback_response.status_code}, content: {playback_response.text}"
+
+@app.route('/top')
+def top_tracks_artists():
+    access_token = request.args.get('access_token')
+    time_range = request.args.get('time_range', 'short_term')  # Default to 'short_term'
+
+    # Set the appropriate time range based on user selection
+    time_ranges = {
+        '1mo': 'short_term',  # Last 4 weeks
+        '6mo': 'medium_term',  # Last 6 months
+        '12mo': 'long_term'  # All time
+    }
+
+    selected_time_range = time_ranges.get(time_range, 'short_term')
+
+    # Fetch top tracks
+    top_tracks_url = f'https://api.spotify.com/v1/me/top/tracks?time_range={selected_time_range}&limit=10'
+    top_tracks_headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    top_tracks_response = requests.get(top_tracks_url, headers=top_tracks_headers)
+
+    # Fetch top artists
+    top_artists_url = f'https://api.spotify.com/v1/me/top/artists?time_range={selected_time_range}&limit=10'
+    top_artists_response = requests.get(top_artists_url, headers=top_tracks_headers)
+
+    if top_tracks_response.status_code == 200 and top_artists_response.status_code == 200:
+        top_tracks = top_tracks_response.json()['items']
+        top_artists = top_artists_response.json()['items']
+
+        # Generate HTML for displaying top tracks and artists
+        track_html = '''
+        <h2>Top Tracks</h2>
+        <ol id="track-list">
+        '''
+        for track in top_tracks:
+            track_html += (
+                f"<li>{track['name']} by {', '.join([artist['name'] for artist in track['artists']])} "
+                f"<button class='play-button' data-track-id='{track['id']}' data-access-token='{access_token}'>Play</button></li>"
+            )
+        track_html += '</ol>'
+
+        artist_html = '<h2>Top Artists</h2><ol>'
+        for artist in top_artists:
+            artist_html += f"<li>{artist['name']}</li>"
+        artist_html += '</ol>'
+
+        # Include JavaScript for playback
+        track_html += '''
+        <script>
+        document.querySelectorAll('.play-button').forEach(button => {
+            button.addEventListener('click', function() {
+                const trackId = this.getAttribute('data-track-id');
+                const accessToken = this.getAttribute('data-access-token');
+
+                fetch(`/play/${trackId}?access_token=${accessToken}`, {
+                    method: 'GET'
+                })
+                .then(response => response.text())
+                .then(data => {
+                    console.log(data);  // You can log or handle the response as needed
+                    updateNowPlaying(this.parentElement);
+                })
+                .catch(error => console.error('Error:', error));
+            });
+        });
+
+        function updateNowPlaying(trackElement) {
+            // Remove 'now-playing' class from any other track
+            document.querySelectorAll('.now-playing').forEach(item => item.classList.remove('now-playing'));
+
+            // Add 'now-playing' class to the clicked track
+            trackElement.classList.add('now-playing');
+
+            // Optionally, update a separate section of the page with the current playing track info
+            const nowPlayingElement = document.getElementById('now-playing');
+            if (nowPlayingElement) {
+                nowPlayingElement.innerHTML = `Now playing: ${trackElement.innerText}`;
+            }
+        }
+        </script>
+        '''
+
+        return track_html + artist_html + '<div id="now-playing">Now playing: None</div>'
+    else:
+        return f"Error retrieving top tracks or artists: {top_tracks_response.json().get('error_description')}"
 
 @app.route('/play/<track_id>')
 def play_track(track_id):
